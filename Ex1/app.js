387a9adb7eeee383281fe9b1a0c07d089b11ea95
Paulo-Cov <paulocov96@gmail.com>
2022-01-19 09:08:07 -0600

    Falata challenge

---
+++ Ex1/app.js
@@ -0,0 +1,6 @@
+function js_style(){
+    document.getElementById("text").style.color="red";
+    document.getElementById("text").style.fontFamily="Arial";
+    document.getElementById("text").style.fontfontSize="60px";
+
+}
